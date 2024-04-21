c944e5c54cfddcb910991c61c8a6ba8bc0aea568
George Fekete <george@primalskill.com>
2024-04-21 13:56:55 +0300

    Implemented []byte and fmt.Stringer interface for slices in dev log

--- append_slice.go
+++ append_slice.go
@@ -7,6 +7,32 @@ import (
 
 func (p *DevHandler) appendSlice(buf []byte, val reflect.Value, fgColor []byte, indent int) []byte {
 
+	// If val implements fmt.Stringer interface, than call it.
+	// This is useful for types like UUID
+
+	stringer := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
+
+	if val.Type().Implements(stringer) == true {
+		valMethod := val.MethodByName("String")
+
+		// Double check if the method value returned is valid
+		if valMethod.Kind() != reflect.Invalid {
+			retVal := valMethod.Call([]reflect.Value{})
+
+			buf = append(buf, retVal[0].String()...)
+			return buf
+		}
+	}
+
+	// []byte is an alias for []uint8 which is a slice, test if it's truly []byte or not
+	if val.CanConvert(reflect.TypeOf([]byte(nil))) {
+		b := val.Bytes()
+		buf = append(buf, string(b)...)
+
+		return buf
+	}
+
+	// Handle it like a regular slice
 	buf = fmt.Appendf(buf, "%s", val.Type().String())
 
 	for i := 0; i < val.Len(); i++ {
